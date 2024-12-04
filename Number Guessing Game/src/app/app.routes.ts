0c84ae7a4b8feb99c54a71010c782766223334dc
Sakshishinare <sakshishinare111@gmail.com>
2024-12-04 15:14:32 +0530

    Add Files

---
+++ Number Guessing Game/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { NumberGuessingComponent } from './number-guessing/number-guessing.component';
+
+export const routes: Routes = [{path:"numberguessing",component:NumberGuessingComponent}];
